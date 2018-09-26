48967d45252f0021bcc0a86ff4454718889a9e09
Luis Rodrigues <goblindegook@users.noreply.github.com>
2018-09-26 18:57:08 +0100

    Search component tests

---
+++ src/components/__tests__/Search.spec.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { render, fireEvent, cleanup } from 'react-testing-library'
+import { Search } from '../Search'
+
+describe('Search', () => {
+  beforeEach(cleanup)
+
+  it('calls onSearch with query value', () => {
+    const onSearchMock = jest.fn(() => [])
+    const renderMock = jest.fn(() => <span />)
+    const value = 'query'
+
+    const { container } = render(<Search onSearch={onSearchMock} render={renderMock} />)
+
+    const input = container.querySelector('[type="search"]') as HTMLInputElement
+    fireEvent.change(input, { target: { value } })
+
+    expect(onSearchMock).toHaveBeenCalledWith(value)
+  })
+
+  it('renders the search results provided by the onSearch function', () => {
+    const results: ReadonlyArray<any> = ['foo', 'bar']
+    const onSearchMock = jest.fn(() => results)
+    const renderMock = (result: any) => <span>{result}</span>
+
+    const { getByText, container } = render(<Search onSearch={onSearchMock} render={renderMock} />)
+
+    const input = container.querySelector('[type="search"]') as HTMLInputElement
+    fireEvent.change(input, { target: { value: 'query' } })
+
+    expect(getByText('foo')).toBeTruthy()
+    expect(getByText('bar')).toBeTruthy()
+  })
+})
